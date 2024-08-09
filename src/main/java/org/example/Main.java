d0f957e0b76c483741792c0c7b6ffb346178c833
santoshnair <santosh.nair712@gmail.com>
2024-08-09 21:10:45 +0530

    main method modified and new test case for handling empty string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
+import java.util.stream.Stream;
 
 public class Main {
+    static int add(String numbers){
+        if(numbers.trim().length()==0){
+            return 0;
+        }
+        return  Stream.of(numbers.split(",")).mapToInt(Integer::parseInt).sum();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String numbers = "1,2,3,4,5,6,7,8,9";
+        int sum = add(numbers);
+        System.out.println("The sum of "+numbers+" is "+sum);
     }
 }
--- src/test/java/AddString.java
+++ src/test/java/AddString.java
@@ -3,13 +3,29 @@ import org.junit.Test;
 import java.util.stream.Stream;
 
 public class AddString {
-    String str = "1,2,3,4,5,6,7,9";
+    String str = "1,2,3,4,5,6,7,8,9";
+    String emptyString = "";
+
     @Test
     public void processStringAddition(){
-        int sum = 37;
+        int sum = 45;
         Assert.assertEquals(sum,this.getStringSum());
     }
+
+    @Test
+    public void processEmptyString(){
+        int sum = 0;
+        Assert.assertEquals(sum,this.getEmptyStringSum());
+    }
+
     private int getStringSum(){
-        return  Stream.of(this.str.split(",")).mapToInt(Integer::parseInt).sum();
+        return Stream.of(this.str.trim().split(",")).mapToInt(Integer::parseInt).sum();
+    }
+
+    private int getEmptyStringSum(){
+        if(this.emptyString.trim().length()==0){
+            return 0;
+        }
+        return Stream.of(this.emptyString.split(",")).mapToInt(Integer::parseInt).sum();
     }
 }
